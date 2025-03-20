b919bc7e1b590a2407edaf789b21407122eacc1b
darkman3134 <darkzman12321@gmail.com>
2025-03-20 15:37:21 +0300

    Add files via upload

---
+++ data/Bot.js
@@ -0,0 +1,7 @@
+const BotData = {
+    token: "",
+    owners: ["1281525006412808192"],
+    clientID: "1317947285874933800"
+}
+
+module.exports = { BotData }
---
+++ data/User.js
@@ -0,0 +1,7 @@
+const UserData = {
+    maxAddLink: 1,
+    premiumMaxAddLink: 20,
+    allowedDomains: ['.com', '.net', '.repl.co', '.glitch.me', '.xyz']
+}
+
+module.exports = { UserData }
